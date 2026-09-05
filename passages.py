4ef031d53339c6197a8061223b95591421b65c0a
Susham <sushampokharel@gmail.com>
2026-09-05 17:07:00 +0545

    Initial Structure

---
+++ passages.py
@@ -0,0 +1,9 @@
+"""Typing passages (intentionally left empty).
+
+The frontend ships with a single hard-coded sample passage so the UI can be
+reviewed. Real, randomly selected passages will be served from here by Flask.
+"""
+
+# TODO: Add the collection of typing passages used by the test.
+# TODO: Implement random passage selection (see logic.get_random_passage).
+PASSAGES = []
